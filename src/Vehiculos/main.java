944a9a182cf7161b58e1cff722fde9ddfb4c13f1
MaYan711 <Mariogolchivo11@hotmail.com>
2021-06-24 15:52:58 -0600

    Se agregan vehiculos

--- src/Vehiculos/main.java
+++ src/Vehiculos/main.java
@@ -9,75 +9,35 @@ public class main {
         ArrayList<String> carros = new ArrayList<>();
         ArrayList tiposAutos = new ArrayList();
         for (int i = 0; i < 20; i++) {
-            System.out.println("1");
-            System.out.println("2");
-            System.out.println("3");
+            System.out.println("\nBIENVENIDO\n");
+            System.out.println("1 es para personas");
             int num = Integer.valueOf(lector.nextLine());
             switch (num) {
 
                 case 1:
-                    System.out.println("INgrese cantidad de personas");
+                    System.out.println("Ingrese cantidad de personas");
                     int cantidad = Integer.valueOf(lector.nextLine());
-                    System.out.println("INgrese cantidad de gasolina");
+                    System.out.println("Ingrese cantidad de gasolina");
                     int gas = Integer.valueOf(lector.nextLine());
-                    System.out.println("INgrese cantidad de velocidad");
+                    System.out.println("Ingrese cantidad de velocidad");
                     double vel = Double.valueOf(lector.nextLine());
-                    System.out.println("INgrese cantidad de aceleracion");
+                    System.out.println("Ingrese cantidad de aceleracion");
                     double ac = Double.valueOf(lector.nextLine());
-                    System.out.println("INgrese cantidad de turbo");
+                    System.out.println("Ingrese cantidad de turbo");
                     int turbo = Integer.valueOf(lector.nextLine());
-                    deCarreras carro = new deCarreras(gas, cantidad, vel, ac, turbo);
-                    if (carro instanceof deCarreras) {
-                        tiposAutos.add("Carro de carreras");
-                    }
-                    carros.add(carro.toString());
-                    break;
-                case 2:
-                    System.out.println("INgrese cantidad de personas");
-                    int cantidad1 = Integer.valueOf(lector.nextLine());
-                    System.out.println("INgrese cantidad de gasolina");
-                    int gas1 = Integer.valueOf(lector.nextLine());
-                    System.out.println("INgrese cantidad de velocidad");
-                    double vel1 = Double.valueOf(lector.nextLine());
-                    System.out.println("INgrese cantidad de aceleracion");
-                    double ac1 = Double.valueOf(lector.nextLine());
-                    System.out.println("INgrese el material a cargar");
-                    String material = (lector.nextLine());
-                    deCarga carro1 = new deCarga(gas1, cantidad1, vel1, ac1, material);
-                    if (carro1 instanceof deCarga) {
-                        tiposAutos.add("Carro de carga");
-                    }
-                        carros.add(carro1.toString());
-                        break;
-                        case 3:
-                            System.out.println("INgrese cantidad de personas");
-                            int cantidad2 = Integer.valueOf(lector.nextLine());
-                            System.out.println("INgrese cantidad de gasolina");
-                            int gas2 = Integer.valueOf(lector.nextLine());
-                            System.out.println("INgrese cantidad de velocidad");
-                            double vel2 = Double.valueOf(lector.nextLine());
-                            System.out.println("INgrese cantidad de aceleracion");
-                            double ac2 = Double.valueOf(lector.nextLine());
-                            boolean trasporte = true;
-                            camionetas carro2 = new camionetas(gas2, cantidad2, vel2, ac2, trasporte);
-                            if (carro2 instanceof camionetas) {
-                                tiposAutos.add("Camioneta");
-
-                                carros.add(carro2.toString());
-                            }
-                                break;
-
-
-
-                    }
-
-
+                    //deCarreras carro = new deCarreras(gas, cantidad, vel, ac, turbo);
+                    //if (carro instanceof deCarreras) {
+                       // tiposAutos.add("Carro de carreras");
+                    //}
+                    //carros.add(carro.toString());
+                    //break;
+                //case 2:
+                              //  break;
+                    //}
             }
             Object carreras = new Object();
             System.out.println(carros.toString());
             System.out.println(tiposAutos);
-
-
-
     }
     }
+}
